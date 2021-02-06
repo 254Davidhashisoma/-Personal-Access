2f98036cf6637aa2839523323e4ba50561fa12ff
254Davidhashisoma <davidhashisoam95@gmail.com>
2021-02-06 17:44:17 +0300

    modifying nav and about pages module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
